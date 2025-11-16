b48087eae300319b615205f30e2b744be1cb3615
Michael Faria <michaelronaldfaria@gmail.com>
2025-11-16 14:14:59 +0000

    Minor code reformatting for readability.

--- js/contact_us.js
+++ js/contact_us.js
@@ -1,13 +1,14 @@
 function sendEmail() {
-    const element = document.getElementById("input_form");
-    element.remove();
+  const element = document.getElementById("input_form");
+  element.remove();
 
-    const subtitle = document.getElementById("subtitle");
-    subtitle.textContent = "E-mail submitted!";
+  const subtitle = document.getElementById("subtitle");
+  subtitle.textContent = "E-mail submitted!";
 
-    const message = document.getElementById("instructions");
-    message.textContent = "Thank you for submitting your message. Our team will get back to you shortly."
+  const message = document.getElementById("instructions");
+  message.textContent =
+    "Thank you for submitting your message. Our team will get back to you shortly.";
 
-    const images = document.getElementById("invisible_images");
-    images.id = "visible_images";
+  const images = document.getElementById("invisible_images");
+  images.id = "visible_images";
 }
--- js/index.js
+++ js/index.js
@@ -1,13 +1,13 @@
 function navigateToOpeningTimes() {
-    //console.log("test");
+  //console.log("test");
 
-    const element = document.getElementById("opening_times_normal");
-    element.id = "opening_times_selected";
+  const element = document.getElementById("opening_times_normal");
+  element.id = "opening_times_selected";
 
-    window.setTimeout(resetOpeningTimes,500);
+  window.setTimeout(resetOpeningTimes, 500);
 }
 
 function resetOpeningTimes() {
-    const element = document.getElementById("opening_times_selected");
-    element.id = "opening_times_normal";
+  const element = document.getElementById("opening_times_selected");
+  element.id = "opening_times_normal";
 }
